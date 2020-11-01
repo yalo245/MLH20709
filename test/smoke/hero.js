d48edd72f8ab948160443ae26cfbcd3655d0ebac
Anar Yessetova <naraailana14@gmail.com>
2020-11-01 13:06:43 -0600

    added tests for type of story section

--- test/smoke/hero.js
+++ test/smoke/hero.js
@@ -11,51 +11,62 @@ describe('My Little Hero', function () {
             expect(title).toEqual('MLH trial');
         });
 
-        it('TC-1.002 Header is present', function () {
+        xit('TC-1.002 Header is present', function () {
             const header = $(sel.header).isDisplayed();
             expect(header).toEqual(true);
         });
 
-        it('TC-1.003 Header is correct', function () {
+        xit('TC-1.003 Header is correct', function () {
             const header = $(sel.header).getText();
             expect(header).toEqual(exp.header);
         });
 
-        it('TC-1.004 Description text is present', function () {
+        xit('TC-1.004 Description text is present', function () {
             const description = $(sel.description).isDisplayed();
             expect(description).toEqual(true);
         });
 
-        it('TC-1.005 Description text = Let\'s create your own HERO! It\'s super easy with our application!', function () {
-            const description = $(sel.description).isClickable();
+        xit('TC-1.005 Description text = Let\'s create your own HERO! It\'s super easy with our application!', function () {
+            const description = $(sel.description).getText();
             expect(description).toEqual(exp.description);
         });
-
     });
 
-    xdescribe('Elements exist', function () {
+    xdescribe("Type of Story", () => {
 
-        it('TC-002 Label for name', function () {
-            const label = $$('.ant-form-item-required')[0].isDisplayed();
+        it("TC-5.001 Label for type of story is present", () => {
+            const label = $$(sel.label)[3].isDisplayed();
             expect(label).toEqual(true);
         });
 
-        it('TC-003 Label for gender', function () {
-            const label = $$('.ant-form-item-required')[1].isDisplayed();
-            expect(label).toEqual(true);
+        it("TC-5.002 Label for type of story = 4. What type of story would you like to read?", () => {
+            const text = $$(sel.label)[3].getAttribute('title');
+            expect(text).toEqual("4. What type of story would you like to read?");
         });
 
-        it('TC-004 Label for age', function () {
-            const label = $$('.ant-form-item-required')[2].isDisplayed();
-            expect(label).toEqual(true);
+        it("TC-5.003 Input field is present", () => {
+            const inputF = $(sel.storyIF).isExisting();
+            expect(inputF).toEqual(true);
         });
 
-        it('TC-005 Label for story', function () {
-            const label = $$('.ant-form-item-required')[3].isDisplayed();
-            expect(label).toEqual(true);
+        it("TC-5.004 Placeholder text = Type of the story", () => {
+            const text = $(sel.storyPlaceholder).getText();
+            expect(text).toEqual("Type of the story");
         });
 
+        it("TC-5.005 Dropdown expands", () => {
+            const elem = $(sel.storyDropdownBtn).click();
+            const genre = $$(sel.storyDropdownOption).length;
+            expect(genre).toEqual(7);
+        });
+
+        it("TC-5.012 Dropdown contains option Comedy", () => {
+            const elem = $(sel.storyDropdownBtn).click();
+            const genre = $$(sel.storyDropdownOption)[6].waitForDisplayed();
+            expect($$(sel.storyDropdownOption)[6].getText()).toEqual("Comedy");
+        });
     });
+
 
 });
 
