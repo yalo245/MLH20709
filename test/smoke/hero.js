7c97c3401e0955c43ff60049f1494472cfca8209
Anar Yessetova <naraailana14@gmail.com>
2020-11-01 13:42:27 -0600

    added Before Hook

--- test/smoke/hero.js
+++ test/smoke/hero.js
@@ -2,31 +2,33 @@ const sel = require('../../data/selectors.json');
 const exp = require('../../data/expected.json');
 
 describe('My Little Hero', function () {
+    before(() => {
+        browser.url('https://qa-apps.netlify.app/app_my_hero');
+    });
 
-    describe('Getting to the page', function () {
+    xdescribe('Getting to the page', function () {
 
         it('TC-1.001 Title is correct ', function () {
-            browser.url('https://qa-apps.netlify.app/app_my_hero');
             let title = browser.getTitle();
             expect(title).toEqual('MLH trial');
         });
 
-        xit('TC-1.002 Header is present', function () {
+        it('TC-1.002 Header is present', function () {
             const header = $(sel.header).isDisplayed();
             expect(header).toEqual(true);
         });
 
-        xit('TC-1.003 Header is correct', function () {
+        it('TC-1.003 Header is correct', function () {
             const header = $(sel.header).getText();
             expect(header).toEqual(exp.header);
         });
 
-        xit('TC-1.004 Description text is present', function () {
+        it('TC-1.004 Description text is present', function () {
             const description = $(sel.description).isDisplayed();
             expect(description).toEqual(true);
         });
 
-        xit('TC-1.005 Description text = Let\'s create your own HERO! It\'s super easy with our application!', function () {
+        it('TC-1.005 Description text = Let\'s create your own HERO! It\'s super easy with our application!', function () {
             const description = $(sel.description).getText();
             expect(description).toEqual(exp.description);
         });
