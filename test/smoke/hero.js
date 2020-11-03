56279383f5496e0b919ee09aeb4ae7b9b365678d
Elena-Sergienko <elena.serg.us@gmail.com>
2020-11-03 12:09:40 -0800

    Updated Story(Try again button)

--- test/smoke/hero.js
+++ test/smoke/hero.js
@@ -1,5 +1,7 @@
 import sel from '../../data/selectors.json';
 import exp from '../../data/expected.json';
+import values from '../../data/values.json';
+
 const val = require('../../data/values.json');
 
 describe('My Little Hero', function () {
@@ -152,9 +154,83 @@ describe('My Little Hero', function () {
         });
 
 
-
     });
 
+    describe('Story', function () {
+
+        it('TC-7.007 Verify that User can read the story after submitting with choice type of story Comedy', function () {
+            browser.url('https://qa-apps.netlify.app/app_my_hero');
+
+            const name = $(sel.nameField).setValue(values.name);
+            const setGender = $$(sel.radioClickHeSHeIt)[1].click();
+            const age = $(sel.ageField).setValue(values.age);
+            const clickTypeStory = $(sel.inputFieldTypeStory).click();
+            const clickType = $$(sel.inputFieldTypeStoryArray)[6].click();
+            const activeButton = $(sel.submitButton).isEnabled();
+            const clickCreate = $(sel.submitButton).click();
+            const textOfStory = $(sel.textOfStory).isDisplayed();
+            expect(textOfStory).toEqual(true);
+            const tryAgainButton = $(sel.tryAgainButton).isDisplayed();
+            expect(tryAgainButton).toEqual(true);
+        });
+
+        it('TC-7.013 Verify that submit button is present', function () {
+            browser.url('https://qa-apps.netlify.app/app_my_hero');
+
+            const name = $(sel.nameField).setValue(values.name);
+            const setGender = $$(sel.radioClickHeSHeIt)[1].click();
+            const age = $(sel.ageField).setValue(values.age);
+            const clickTypeStory = $(sel.inputFieldTypeStory).click();
+            const clickType = $$(sel.inputFieldTypeStoryArray)[6].click();
+            const activeButton = $(sel.submitButton).isEnabled();
+            const clickCreate = $(sel.submitButton).click();
+            const tryAgainButton = $(sel.tryAgainButton).isDisplayed();
+            expect(tryAgainButton).toEqual(true);
+        });
+
+        it('TC-7.014 Verify that submit button name is Try again!', function () {
+            browser.url('https://qa-apps.netlify.app/app_my_hero');
+
+            const name = $(sel.nameField).setValue(values.name);
+            const setGender = $$(sel.radioClickHeSHeIt)[1].click();
+            const age = $(sel.ageField).setValue(values.age);
+            const clickTypeStory = $(sel.inputFieldTypeStory).click();
+            const clickType = $$(sel.inputFieldTypeStoryArray)[6].click();
+            const activeButton = $(sel.submitButton).isEnabled();
+            const clickCreate = $(sel.submitButton).click();
+            const tryAgainButton = $(sel.tryAgainButton).getText();
+            expect(tryAgainButton).toEqual(exp.tryAgain);
+        });
+
+        it('TC-7.016 Verify that submit button is active (clickable)', function () {
+            browser.url('https://qa-apps.netlify.app/app_my_hero');
+
+            const name = $(sel.nameField).setValue(values.name);
+            const setGender = $$(sel.radioClickHeSHeIt)[1].click();
+            const age = $(sel.ageField).setValue(values.age);
+            const clickTypeStory = $(sel.inputFieldTypeStory).click();
+            const clickType = $$(sel.inputFieldTypeStoryArray)[6].click();
+            const activeButton = $(sel.submitButton).isEnabled();
+            const clickCreate = $(sel.submitButton).click();
+            const tryAgainButton = $(sel.tryAgainButton).isClickable();
+            expect(tryAgainButton).toEqual(true);
+        });
+
+        it('TC-7.017 Verify that submit button refreshes the page', function () {
+            browser.url('https://qa-apps.netlify.app/app_my_hero');
+
+            const name = $(sel.nameField).setValue(values.name);
+            const setGender = $$(sel.radioClickHeSHeIt)[1].click();
+            const age = $(sel.ageField).setValue(values.age);
+            const clickTypeStory = $(sel.inputFieldTypeStory).click();
+            const clickType = $$(sel.inputFieldTypeStoryArray)[6].click();
+            const activeButton = $(sel.submitButton).isEnabled();
+            const clickCreate = $(sel.submitButton).click();
+            const tryAgainButton = $(sel.tryAgainButton).click();
+            const homePageDescription = $(sel.description).getText()
+            expect(homePageDescription).toEqual(exp.description);
+        });
+    });
 
 });
 
